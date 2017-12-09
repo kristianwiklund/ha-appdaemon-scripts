f51af27f6ed6bbb6c718f686c1c59afaa37c8dad
Kristian Wiklund <githubkristian@snabela.nl>
2017-12-09 14:41:59 +0100

    added configuration arguments to the engine block heater app

--- motorvarmare.py
+++ motorvarmare.py
@@ -5,8 +5,15 @@ import datetime
 import time
 
 # this uses the google calendar component
-# assumption is that the calendar is called "calendar.motorvarmare" (could be set in a parameter, but that is for later...)
 
+# how to config
+#motorvarmare:
+#    module: motorvarmare
+#    class: Motorvarmare
+#    calendar: calendar.motorvarmare
+#    offset: 120
+#    switch: switch.motorvarmare
+    
 
 
 class Motorvarmare(appapi.AppDaemon):
@@ -18,7 +25,7 @@ class Motorvarmare(appapi.AppDaemon):
         attrib = state["attributes"]
         deadline = parser.parse(attrib["start_time"])
         #print(deadline)
-        self.starttime = deadline - datetime.timedelta(minutes=120)
+        self.starttime = deadline - datetime.timedelta(minutes=self.offset)
         print("motorvärmare: setting timer to "+str(self.starttime))
         self.stoptime = deadline + datetime.timedelta(minutes=30)
         
@@ -33,11 +40,11 @@ class Motorvarmare(appapi.AppDaemon):
         self.handlestop = self.run_at(self.turnoff, self.stoptime)
 
     def turnon(self, kwargs):
-        self.turn_on("switch.motorvarmare")
+        self.turn_on(self.switch)
         self.handlestart = False
 
     def turnoff(self, kwargs):
-        self.turn_off("switch.motorvarmare")
+        self.turn_off(self.switch)
         self.handlestop = False
     
             
@@ -45,8 +52,11 @@ class Motorvarmare(appapi.AppDaemon):
         self.handlestart = False
         self.handlestop = False        
 
+        self.offset = self.args["offset"]
+        self.sensor = self.args["calendar"]
+        self.switch = self.args["switch"]
         
-        self.sensor = "calendar.motorvarmare"
+        # self.sensor = "calendar.motorvarmare"
         self.listen_state(self.checkstate, self.sensor)
         state = self.get_state(self.sensor, attribute = "all")
         self.updatetimers(state)
