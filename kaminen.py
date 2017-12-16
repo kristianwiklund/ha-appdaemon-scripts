36e5dec795139a78bc610284ec26bb0392dfadbf
Kristian Wiklund <githubkristian@snabela.nl>
2017-12-16 23:30:42 +0100

    fix to stop spamming the log with turn on/off messages for the fireplace fan

--- kaminen.py
+++ kaminen.py
@@ -5,8 +5,10 @@ import appdaemon.appapi as appapi
 class Kaminflakt(appapi.AppDaemon):
 
     def initialize(self):
+        self.state = "baff"
         self.handle = self.run_in(self.check_temperature, 1)
-
+        self.state = self.get_state("switch.kaminflakten")
+        log ("Kaminfläkten is "+str(self.state))
 
     def check_temperature(self,kwargs):
         self.handle = self.run_in(self.check_temperature,60)
@@ -14,9 +16,12 @@ class Kaminflakt(appapi.AppDaemon):
 
         t = float(self.get_state("sensor.kaminen_temperature"))
         if t>40.0:
-            self.turn_on("switch.kaminflakten")
+            if self.state != "on":
+                self.turn_on("switch.kaminflakten")
+                self.state = "on"
         else:
-            self.turn_off("switch.kaminflakten")            
-
+            if self.state != "off":
+                self.turn_off("switch.kaminflakten")            
+                self.state = "off"
 
         
